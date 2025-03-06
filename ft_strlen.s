e80590804153fb5edb28482ab31eb044be354609
Flora <flora.co23@gmail.com>
2025-03-06 11:39:16 -0500

    ft_strlen

---
+++ ft_strlen.s
@@ -0,0 +1,16 @@
+
+section .text:
+    global ft_strlen
+
+ft_strlen:
+    mov rcx, 0
+
+.loop: ; label local
+    cmp [rdi + rcx], 0
+    je .end
+    inc rcx
+    jmp .loop
+
+.end:
+    mov rax, rcx
+    ret
