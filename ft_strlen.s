6ae15314f86f4cd2ca296cfcc0dd99200db23a59
Flora <flora.co23@gmail.com>
2025-03-06 12:32:25 -0500

    save

--- ft_strlen.s
+++ ft_strlen.s
@@ -3,14 +3,20 @@ section .text:
     global ft_strlen
 
 ft_strlen:
-    mov rcx, 0
+    test rdi, rdi ; similaire a AND mais ne modifie pas les operande (test si ptr = null)
+    je .null_case
+    xor rcx, rcx ; toujours faire ça pour mettre un registre a 0
 
 .loop: ; label local
-    cmp [rdi + rcx], 0
+    cmp byte [rdi + rcx], 0
     je .end
     inc rcx
     jmp .loop
 
 .end:
     mov rax, rcx
+    ret
+
+.null_case:
+    xor rax, rax
     ret
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+size_t ft_strlen(const char *str);
+
+int main() {
+
+    //strlen
+    printf("%zu\n", ft_strlen("Hello, world!"));
+    return 0;
+}
